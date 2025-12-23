504a154f086c9bb6193a0666c7d00906e4c00406
Devajayantha <devajayantha888@gmail.com>
2025-12-23 12:12:38 +0800

    update seeder & add timezone on env

--- database/seeders/CategoriesTableSeeder.php
+++ database/seeders/CategoriesTableSeeder.php
@@ -20,6 +20,10 @@ class CategoriesTableSeeder extends Seeder
                 'type' => CategoryType::Income->value,
             ],
             [
+                'name' => 'Freelance',
+                'type' => CategoryType::Income->value,
+            ],
+            [
                 'name' => 'Food',
                 'type' => CategoryType::Expense->value,
             ],
@@ -27,6 +31,94 @@ class CategoriesTableSeeder extends Seeder
                 'name' => 'Transport',
                 'type' => CategoryType::Expense->value,
             ],
+            [
+                'name' => 'Entertainment',
+                'type' => CategoryType::Expense->value,
+            ],
+            [
+                'name' => 'Investment',
+                'type' => CategoryType::Expense->value,
+            ],
+            [
+                'name' => 'Health',
+                'type' => CategoryType::Expense->value,
+            ],
+            [
+                'name' => 'Fruits',
+                'type' => CategoryType::Expense->value,
+            ],
+            [
+                'name' => 'Bills',
+                'type' => CategoryType::Expense->value,
+            ],
+            [
+                'name' => 'Clothes',
+                'type' => CategoryType::Expense->value,
+            ],
+            [
+                'name' => 'Travel',
+                'type' => CategoryType::Expense->value,
+            ],
+            [
+                'name' => 'Education',
+                'type' => CategoryType::Expense->value,
+            ],
+            [
+                'name' => 'Gift',
+                'type' => CategoryType::Expense->value,
+            ],
+            [
+                'name' => 'Pets',
+                'type' => CategoryType::Expense->value,
+            ],
+            [
+                'name' => 'Motor',
+                'type' => CategoryType::Expense->value,
+            ],
+            [
+                'name' => 'Car',
+                'type' => CategoryType::Income->value,
+            ],
+            [
+                'name' => 'Home',
+                'type' => CategoryType::Expense->value,
+            ],
+            [
+                'name' => 'Assurance',
+                'type' => CategoryType::Expense->value,
+            ],
+            [
+                'name' => 'Sports',
+                'type' => CategoryType::Expense->value,
+            ],
+            [
+                'name' => 'Social',
+                'type' => CategoryType::Expense->value,
+            ],
+            [
+                'name' => 'Subscription',
+                'type' => CategoryType::Expense->value,
+            ],
+            [
+                'name' => 'Transportation',
+                'type' => CategoryType::Expense->value,
+            ],
+            [
+                'name' => 'Beauty',
+                'type' => CategoryType::Expense->value,
+            ],
+            [
+                'name' =>  'Electronics',
+                'type' => CategoryType::Expense->value,
+            ],
+            [
+                'name' => 'Shopping',
+                'type' => CategoryType::Expense->value,
+            ],
+            [
+                'name' => 'Others',
+                'type' => CategoryType::Expense->value,
+            ]
         ];
 
         foreach ($categories as $data) {
@@ -36,6 +128,7 @@ class CategoriesTableSeeder extends Seeder
                     'name' => $data['name'],
                 ],
                 [
+                    'name' => $data['name'],
                     'type' => $data['type'],
                 ],
             );
